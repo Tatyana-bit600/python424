c2937c81cd74fd8b5d73c5f02a7b26636cef9693
Tatyana-bit600 <03767w@gmail.com>
2025-03-06 14:40:09 +0300

    Add files via upload

---
+++ PythonFastAPI/fastapiredis/tests.py
@@ -0,0 +1,29 @@
+from unittest import mock
+
+import pytest
+from httpx import ASGITransport, AsyncClient
+
+from .application import app, container
+from .services import Service
+
+
+@pytest.fixture
+def client(event_loop):
+    client = AsyncClient(
+        transport=ASGITransport(app=app),
+        base_url="http://test"http://test"http://test"http://test"http://test"http://test",
+    )
+    yield client
+    event_loop.run_until_complete(client.aclose())
+
+
+@pytest.mark.asyncio
+async def test_index(client):
+    service_mock = mock.AsyncMock(spec=Service)
+    service_mock.process.return_value = "Foo"
+
+    with container.service.override(service_mock):
+        response = await client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {"result": "Foo"}
